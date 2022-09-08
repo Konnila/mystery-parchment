bc74841faa423ae9a0fc49b42544f0a5a7a20225
Toni Könnilä <toni_konnila@hotmail.com>
2022-09-08 20:04:57 +0300

    saving commit

---
+++ src/parchment/core.clj
@@ -0,0 +1,51 @@
+(ns parchment.core
+  (:gen-class)
+  (:require [clojure.java.io :refer [resource]]))
+
+(use 'mikera.image.core)
+(use 'mikera.image.colours)
+(require '[mikera.image.filters :as filt])
+
+(defn get-resource [path-to-file]
+  (-> path-to-file resource load-image))
+
+(defn get-image-pixels [img]
+  (get-pixels img))
+
+(def secret-message
+  (str 
+    "OCDNDNVNZXMZOGDNOJANKZGGNOCVODCVQZDIQZIOZYHTN"
+    "ZGAGVIBGJXFBGPZNOCZOJIBPZOJOCZMJJAJAOCZHJPOCGZ" 
+    "QDXJMKPNGDAONOCZCZQDXODHDIOJOCZVDMWTOCZDMVIFGZ" 
+    "GDWZMVXJMKPNDNVXJPIOZMEDISAJMGZQDXJMKPNHPAAGDV" 
+    "OJADGGNDONOVMBZONZVMNRDOCPIDYZIODADVWGZWPUUDIB" 
+    "NZXOPHNZHKMVDNAJMZIZHDZN"))
+
+(defn replace-pixels-for-color
+  "Given a primitive int of color to replace, replaces it with a black pixel"
+  [file primitive-int-of-color-to-replace]
+  (let [pixels (get-image-pixels file)]
+    (dotimes [i (count pixels)]
+      (let [rgbint (aget pixels i)]
+        (when (= rgbint primitive-int-of-color-to-replace) (aset pixels i (rgb-from-components 0 0 0)))))
+    (set-pixels file pixels)))
+
+(defn decrypt-caesar-message 
+  "assumes shifts-to-right to be between 0 and 25." 
+  [message shifts-to-right]
+  (let [alphabet (map char "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        shifted-alphabet (->> (cycle alphabet) (take 52) (drop shifts-to-right) (map char))
+        relation-zipmap (zipmap alphabet shifted-alphabet)] ;map the original alphabets to our new shifted alphabet for easy lookup with a key-value map
+        (apply str (map #(relation-zipmap %) message)))) ;map every char to the corresponding value present in relation-zipmap and turn the whole deciphered vector into a string
+
+(defn -main
+  "Solves mysteries with ease!"
+  [& args]
+    (let [mysterious-file-buffer (get-resource "parchment.png")]
+      ;Note: -199194 hardcoded primitive int that I found out by manual testing the rgb colors of the pixels in this file.
+      ;There was one another color very close, but I picked this one since it was the non-prevalent one of the two.
+      (replace-pixels-for-color mysterious-file-buffer -199194)
+      (show mysterious-file-buffer)
+      ;; now we can see the mysterious message, let's magically decipher with 21 shifts to right. Not like I tested this to be the right one or anything.
+      (println (decrypt-caesar-message secret-message 5))))
+
---
+++ test/parchment/core_test.clj
@@ -0,0 +1,7 @@
+(ns parchment.core-test
+  (:require [clojure.test :refer :all]
+            [parchment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
