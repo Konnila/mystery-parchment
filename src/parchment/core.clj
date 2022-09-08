287dfc1611466340f849469fb2c88f590c68f008
Toni Könnilä <toni_konnila@hotmail.com>
2022-09-08 21:19:54 +0300

    remove unused code

--- src/parchment/core.clj
+++ src/parchment/core.clj
@@ -4,7 +4,6 @@
 
 (use 'mikera.image.core)
 (use 'mikera.image.colours)
-(require '[mikera.image.filters :as filt])
 
 (defn get-resource [path-to-file]
   (-> path-to-file resource load-image))
@@ -36,7 +35,7 @@
   (let [alphabet (map char "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
         shifted-alphabet (->> (cycle alphabet) (take 52) (drop shifts-to-right) (map char))
         relation-zipmap (zipmap alphabet shifted-alphabet)] ;map the original alphabets to our new shifted alphabet for easy lookup with a key-value map
-        (apply str (map #(relation-zipmap %) message)))) ;map every char to the corresponding value present in relation-zipmap and turn the whole deciphered vector into a string
+          (apply str (map #(relation-zipmap %) message)))) ;map every char to the corresponding value present in relation-zipmap and turn the whole deciphered vector into a string
 
 (defn -main
   "Solves mysteries with ease!"
